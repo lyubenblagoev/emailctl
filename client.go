839c692e0f68907702267d645ed11f49ceafcedd
Lyuben Blagoev <lblagoev@gmail.com>
2017-10-05 08:36:08 +0300

    Implement Postfix REST Server API client over goprsc.Client

---
+++ client.go
@@ -0,0 +1,42 @@
+package emailctl
+
+import (
+	"fmt"
+
+	"github.com/lyubenblagoev/goprsc"
+	"github.com/spf13/viper"
+)
+
+// Client is the Postfix REST server client.
+type Client struct {
+	client *goprsc.Client
+}
+
+// NewClient creates an instance of Client.
+func NewClient() (*Client, error) {
+	goprscClient, err := getGoprscClient()
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize Postfix REST Server API client: %s", err)
+	}
+
+	client := &Client{
+		client: goprscClient,
+	}
+
+	return client, nil
+}
+
+func getGoprscClient() (*goprsc.Client, error) {
+	host := viper.GetString("host")
+	port := viper.GetString("port")
+	useHTTPS := viper.GetBool("https")
+
+	var options []goprsc.ClientOption
+	options = append(options, goprsc.HostOption(host))
+	options = append(options, goprsc.PortOption(port))
+	if useHTTPS {
+		options = append(options, goprsc.HTTPSProtocolOption())
+	}
+
+	return goprsc.NewClientWithOptions(nil, options...)
+}
